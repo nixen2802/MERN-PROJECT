7dc3d5f9183140c375ae74cfe47f075f0ef4644b
Hussein <hussein.m@somaiya.edu>
2021-11-03 23:20:00 +0530

    register and login page styles

--- billing-system/src/Login.js
+++ billing-system/src/Login.js
@@ -1,6 +1,7 @@
 import "./Login.css";
 import React, { Component } from "react";
 import axios from "axios";
+import img_logo from "./images/img-01.png";
 class Register extends Component {
 	constructor(props) {
 		super(props);
@@ -55,37 +56,86 @@ class Register extends Component {
 	render() {
 		return (
 			<div>
-				<br />
-				<div className="container">
-					<form onSubmit={this.handleSubmit}>
-						<div style={{ width: "30%" }} className="form-group">
-							<input
-								type="text"
-								className="form-control"
-								name="email"
-								placeholder="Email"
-								onChange={this.handleInputChange}
-								value={this.state.email}
-							/>
+				<div class="limiter">
+					<div class="container-login100">
+						<div class="wrap-login100">
+							<div class="login100-pic js-tilt" data-tilt="">
+								<img src={img_logo} alt="IMG" />
+							</div>
+							<form
+								class="login100-form validate-form"
+								onSubmit={this.handleSubmit}
+							>
+								<span class="login100-form-title">Login</span>
+								<div
+									class="wrap-input100 validate-input"
+									data-validate="Valid email is required: ex@abc.xyz"
+								>
+									<input
+										class="input100"
+										type="email"
+										name="email"
+										placeholder="Email"
+										onChange={this.handleInputChange}
+										value={this.state.email}
+									/>
+									<span class="focus-input100"></span>
+									<span class="symbol-input100">
+										<i
+											class="fa fa-envelope"
+											aria-hidden="true"
+										></i>
+									</span>
+								</div>
+								<div
+									class="wrap-input100 validate-input"
+									data-validate="Password is required"
+								>
+									<input
+										class="input100"
+										type="password"
+										name="password"
+										placeholder="Password"
+										onChange={this.handleInputChange}
+										value={this.state.password}
+									/>
+									<span class="focus-input100"></span>
+									<span class="symbol-input100">
+										<i
+											class="fa fa-lock"
+											aria-hidden="true"
+										></i>
+									</span>
+								</div>
+								<div class="container-login100-form-btn">
+									<button class="login100-form-btn">
+										Login
+									</button>
+								</div>
+								<div
+									class="text-center"
+									style={{ paddingTop: "12px" }}
+								>
+									<span class="txt1">Forgot</span>
+									<a class="txt2" href="#">
+										Username / Password?
+									</a>
+								</div>
+								<div
+									class="text-center"
+									style={{ paddingTop: "136px" }}
+								>
+									<a class="txt2" href="#">
+										Create your Account
+										<i
+											class="fa fa-long-arrow-right m-l-5"
+											aria-hidden="true"
+										></i>
+									</a>
+								</div>
+							</form>
 						</div>
-						<br />
-						<div style={{ width: "30%" }} className="form-group">
-							<input
-								type="text"
-								className="form-control"
-								name="password"
-								placeholder="Password"
-								onChange={this.handleInputChange}
-								value={this.state.password}
-							/>
-						</div>
-						<br />
-						<div style={{ width: "30%" }}>
-							<button className="btn btn-success" type="submit">
-								Login
-							</button>
-						</div>
-					</form>
+					</div>
 				</div>
 			</div>
 		);
