a1237ed22f2076eff5f4d6779555a2369ad12f26
Hussein <hussein.m@somaiya.edu>
2021-11-12 21:20:26 +0530

    converted bill amount to words

--- billing-system/src/Showbill.js
+++ billing-system/src/Showbill.js
@@ -16,6 +16,154 @@ import { Link, useParams, withRouter } from "react-router-dom";
 import React from "react";
 import axios from "axios";
 import number_to_word from "number-to-words";
+
+function numberToEnglish(n, custom_join_character) {
+	var string = n.toString(),
+		units,
+		tens,
+		scales,
+		start,
+		end,
+		chunks,
+		chunksLen,
+		chunk,
+		ints,
+		i,
+		word,
+		words;
+
+	var and = custom_join_character || "and";
+
+	/* Is number zero? */
+	if (parseInt(string) === 0) {
+		return "zero";
+	}
+
+	/* Array of units as words */
+	units = [
+		"",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eighteen",
+		"nineteen",
+	];
+
+	/* Array of tens as words */
+	tens = [
+		"",
+		"",
+		"twenty",
+		"thirty",
+		"forty",
+		"fifty",
+		"sixty",
+		"seventy",
+		"eighty",
+		"ninety",
+	];
+
+	/* Array of scales as words */
+	scales = [
+		"",
+		"thousand",
+		"million",
+		"billion",
+		"trillion",
+		"quadrillion",
+		"quintillion",
+		"sextillion",
+		"septillion",
+		"octillion",
+		"nonillion",
+		"decillion",
+		"undecillion",
+		"duodecillion",
+		"tredecillion",
+		"quatttuor-decillion",
+		"quindecillion",
+		"sexdecillion",
+		"septen-decillion",
+		"octodecillion",
+		"novemdecillion",
+		"vigintillion",
+		"centillion",
+	];
+
+	/* Split user arguemnt into 3 digit chunks from right to left */
+	start = string.length;
+	chunks = [];
+	while (start > 0) {
+		end = start;
+		chunks.push(string.slice((start = Math.max(0, start - 3)), end));
+	}
+
+	/* Check if function has enough scale words to be able to stringify the user argument */
+	chunksLen = chunks.length;
+	if (chunksLen > scales.length) {
+		return "";
+	}
+
+	/* Stringify each integer in each chunk */
+	words = [];
+	for (i = 0; i < chunksLen; i++) {
+		chunk = parseInt(chunks[i]);
+
+		if (chunk) {
+			/* Split chunk into array of individual integers */
+			ints = chunks[i].split("").reverse().map(parseFloat);
+
+			/* If tens integer is 1, i.e. 10, then add 10 to units integer */
+			if (ints[1] === 1) {
+				ints[0] += 10;
+			}
+
+			/* Add scale word if chunk is not zero and array item exists */
+			if ((word = scales[i])) {
+				words.push(word);
+			}
+
+			/* Add unit word if array item exists */
+			if ((word = units[ints[0]])) {
+				words.push(word);
+			}
+
+			/* Add tens word if array item exists */
+			if ((word = tens[ints[1]])) {
+				words.push(word);
+			}
+
+			/* Add 'and' string after units or tens integer if: */
+			if (ints[0] || ints[1]) {
+				/* Chunk has a hundreds integer or chunk is the first of multiple chunks */
+				if (ints[2] || (!i && chunksLen)) {
+					words.push(and);
+				}
+			}
+
+			/* Add hundreds word if array item exists */
+			if ((word = units[ints[2]])) {
+				words.push(word + " hundred");
+			}
+		}
+	}
+
+	return words.reverse().join(" ");
+}
 class Showbill extends React.Component {
 	constructor(props) {
 		super(props);
@@ -473,7 +621,7 @@ class Showbill extends React.Component {
 							</tr>
 							<tr>
 								<td style={{ width: "50%" }} rowSpan={2}>
-									{number_to_word.toWords(
+									{numberToEnglish(
 										Math.round(
 											this.state.total_amount +
 												this.state.sgst_amount +
@@ -518,7 +666,7 @@ class Showbill extends React.Component {
 							<td style={{ width: "40%" }}>
 								{" "}
 								<span contentEditable>
-									GST NO:27AAAFT1127D1ZP
+									GST NO: {this.state.gst_no}
 								</span>
 							</td>
 							<td> </td>
