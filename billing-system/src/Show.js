08dc616c21c3949750a355145cb0b8cda9fc13fb
Hussein <hussein.m@somaiya.edu>
2021-11-12 13:19:26 +0530

    UI changes in show and showbill pages

--- billing-system/src/Show.js
+++ billing-system/src/Show.js
@@ -8,7 +8,7 @@ export default class Detials extends React.Component {
 		this.state = {
 			value: this.props.location.state,
 			bills: [],
-			customer: []
+			customer: [],
 		};
 	}
 	componentDidMount() {
@@ -21,63 +21,87 @@ export default class Detials extends React.Component {
 	render() {
 		const values = this.state.value;
 		const bill = this.state.bills.map((bill) => (
-			<div key={bill._id}>
-			<Link
-			to={{
-				pathname: `/showbill/${bill.billnumber}`,
-				state: values,
-			}}>
-				<div>
-					<p>Bill Number : {bill.billnumber}</p>
-					<p>Customer Name : {bill.customer_name}</p>
-					<p>Date Of Supply : {bill.date_of_supply}</p>
-					<p>Total Amount : {bill.total_amount}</p>
-					<p>GST : {bill.gst}</p>
-					<p>Place Of Supply : {bill.place_of_supply}</p>
-					<p>Transporter Info : {bill.transporter_info}</p>
-					<p>GST NO : {bill.gst_no}</p>
-					<p>Billing address : {bill.billing_address}</p>
-				</div>
-			</Link>
-			</div>
-			// <div className="col-lg-3 col-md-12 col-xl-3">
-			// 	<div class="card" key={bill._id} style={{ marginTop: "15px" }}>
-			// 		<div class="card-header">
-			// 			Bill Number : {bill.billlnumber}
+			// <div key={bill._id}>
+			// 	<Link
+			// 		to={{
+			// 			pathname: `/showbill/${bill.billnumber}`,
+			// 			state: values,
+			// 		}}
+			// 	>
+			// 		<div>
+			// 			<p>Bill Number : {bill.billnumber}</p>
+			// 			<p>Customer Name : {bill.customer_name}</p>
+			// 			<p>Date Of Supply : {bill.date_of_supply}</p>
+			// 			<p>Total Amount : {bill.total_amount}</p>
+			// 			<p>GST : {bill.gst}</p>
+			// 			<p>Place Of Supply : {bill.place_of_supply}</p>
+			// 			<p>Transporter Info : {bill.transporter_info}</p>
+			// 			<p>GST NO : {bill.gst_no}</p>
+			// 			<p>Billing address : {bill.billing_address}</p>
 			// 		</div>
-			// 		<div class="card-body">
-			// 			<h5 class="card-title">{bill.company_name}</h5>
-			// 			<p class="card-text">
-			// 				With supporting text below as a natural lead-in to
-			// 				additional content.
-			// 			</p>
-			// 			<Link
-			// 				to={{
-			// 					pathname: `/showbill/${bill.billlnumber}`,
-			// 					state: values,
-			// 				}}
-			// 				className="btn"
-			// 				style={{
-			// 					backgroundColor: "#9053c7",
-			// 					color: "white",
-			// 				}}
-			// 			>
-			// 				₹{bill.total_amount}
-			// 			</Link>
-			// 		</div>
-			// 		<div class="card-footer">
-			// 			<Link
-			// 				to={{
-			// 					pathname: `/showbill/${bill.billlnumber}`,
-			// 					state: values,
-			// 				}}
-			// 				className="btn btn-outline-primary"
-			// 			>
-			// 				More Info
-			// 			</Link>
-			// 		</div>
-			// 	</div>
+			// 	</Link>
 			// </div>
+			<div className="col-xl-6 col-lg-6 col-md-12 col-xl-3">
+				<div
+					class="card customShadow"
+					key={bill._id}
+					style={{ marginTop: "15px" }}
+				>
+					<div class="card-header">
+						Bill Number : {bill.billnumber}
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">{bill.company_name}</h5>
+						<div class="billDetails">
+							<div className="billDetailTags">
+								<p>Customer Name : </p>
+								<p>Date Of Supply : </p>
+								<p>Total Amount : </p>
+								<p>GST : </p>
+								<p>Place Of Supply : </p>
+								<p>Transporter Info : </p>
+								<p>GST NO : </p>
+								<p>Billing address : </p>
+							</div>
+							<div className="billDetailsValues">
+								<p>{bill.customer_name}</p>
+								<p>{bill.date_of_supply}</p>
+								<p>{bill.total_amount}</p>
+								<p>{bill.gst}</p>
+								<p>{bill.place_of_supply}</p>
+								<p>{bill.transporter_info}</p>
+								<p>{bill.gst_no}</p>
+								<p>{bill.billing_address}</p>
+							</div>
+						</div>
+						<Link
+							to={{
+								pathname: `/showbill/${bill.billnumber}`,
+								state: values,
+							}}
+							className="btn"
+							style={{
+								backgroundColor: "#9053c7",
+								color: "white",
+								marginTop: "20px",
+							}}
+						>
+							₹{bill.total_amount}
+						</Link>
+					</div>
+					<div class="card-footer">
+						<Link
+							to={{
+								pathname: `/showbill/${bill.billnumber}`,
+								state: values,
+							}}
+							className="btn btn-outline-primary"
+						>
+							More Info
+						</Link>
+					</div>
+				</div>
+			</div>
 		));
 		function renderElement() {
 			if (values.email === "nayanmandaliya01@gmail.com") {
@@ -88,6 +112,7 @@ export default class Detials extends React.Component {
 								pathname: "/showproducts",
 								state: values, // your data array of objects
 							}}
+							style={{ marginRight: "15px" }}
 						>
 							<button className="btn btn-outline-secondary">
 								View Products
@@ -110,7 +135,7 @@ export default class Detials extends React.Component {
 		}
 
 		return (
-			<div className="container" style={{ margin: "0px" }}>
+			<div className="container mx-auto" style={{ margin: "0px" }}>
 				<div className="row row-content">{bill}</div>
 				<div>
 					<Link
@@ -122,24 +147,26 @@ export default class Detials extends React.Component {
 						style={{
 							backgroundColor: "#fcf93c",
 							marginTop: "15px",
+							marginRight: "15px",
+							marginBottom: "15px",
 						}}
 					>
 						Add bill
 					</Link>
-				</div>
-				<div>{renderElement()}</div>
-				<div>
 					<Link
 						to={"/"}
 						className="btn"
 						style={{
 							backgroundColor: "#fcf93c",
 							marginTop: "15px",
+							marginRight: "15px",
+							marginBottom: "15px",
 						}}
 					>
 						Logout
 					</Link>
 				</div>
+				<div>{renderElement()}</div>
 			</div>
 		);
 	}
--- billing-system/src/Showbill.js
+++ billing-system/src/Showbill.js
@@ -24,6 +24,7 @@ class Showbill extends React.Component {
 			value: this.props.location.state,
 			bills: [],
 			bill_details: [],
+			total_amount: 0,
 		};
 	}
 	componentDidMount() {
@@ -45,50 +46,165 @@ class Showbill extends React.Component {
 				this.setState({
 					bill_details: result.data,
 				});
+				console.log("Somtsdvubsivdusuvdsvdviosdocvsnodcn");
 				console.log(this.state.bill_details);
+				this.setState({
+					total_amount: this.state.bill_details[0].total_amount,
+				});
 			});
 	}
-	print(){
+	print() {
 		window.print();
 	}
 	render() {
 		const values = this.state.value;
 		const billdetails = this.state.bill_details.map((bill) => (
-			<div style={{ border: "1px solid black" }} key={bill._id}>
-				<p>Bill Number : {bill.billnumber}</p>
-				<p>Customer Name : {bill.customer_name}</p>
-				<p>Date Of Supply : {bill.date_of_supply}</p>
-				<p>Total Amount : {bill.total_amount}</p>
-				<p>GST : {bill.gst}</p>
-				<p>Place Of Supply : {bill.place_of_supply}</p>
-				<p>Transporter Info : {bill.transporter_info}</p>
-				<p>GST NO : {bill.gst_no}</p>
-				<p>Billing address : {bill.billing_address}</p>
+			<div key={bill._id}>
+				<div className="card-header">
+					Bill Number : {bill.billnumber}
+				</div>
+				<div className="card-body">
+					<h5 className="card-title" style={{ marginBottom: "6%" }}>
+						{bill.company_name}
+					</h5>
+					<div class="billdetails">
+						<div className="billDetailTags">
+							<p>Customer Name : </p>
+							<p>Date Of Supply : </p>
+							<p>Total Amount : </p>
+							<p>GST : </p>
+							<p>Place Of Supply : </p>
+							<p>Transporter Info : </p>
+							<p>GST NO : </p>
+							<p>Billing address : </p>
+						</div>
+						<div className="billDetailsValues">
+							<p>{bill.customer_name}</p>
+							<p>{bill.date_of_supply}</p>
+							<p>{bill.total_amount}</p>
+							<p>{bill.gst}</p>
+							<p>{bill.place_of_supply}</p>
+							<p>{bill.transporter_info}</p>
+							<p>{bill.gst_no}</p>
+							<p>{bill.billing_address}</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		));
 		const bill = this.state.bills.map((bill) => (
-			<div style={{ border: "1px solid black" }} key={bill._id}>
-				<h3>Product Name : {bill.product_name}</h3>
-				<p>Quantity : {bill.quantity}</p>
-				<p>Amount : {bill.amount}</p>
+			<div className="row row-content" key={bill._id}>
+				<div
+					class="placement
+						col-lg-4
+						col-md-4
+						col-sm-4
+						billItems
+					"
+				>
+					{bill.product_name}
+				</div>
+				<div
+					class="placement
+						col-lg-4
+						col-md-4
+						col-sm-4
+						"
+				>
+					{bill.quantity}
+				</div>
+				<div
+					class="placement
+						col-lg-4
+						col-md-4
+						col-sm-4
+					"
+				>
+					{bill.amount}
+				</div>
 			</div>
 		));
 
 		return (
-			<div>
-				{billdetails}
-				{bill}
+			<div className="container" style={{ marginTop: "8%" }}>
+				<div className="row row-content">
+					<div className="col-12">
+						<h1 style={{ textAlign: "center", marginBottom: "2%" }}>
+							Final Bill
+						</h1>
+						<div className="mainBill">
+							<div
+								className="customShadow card customCard"
+								style={{ borderRadius: "6px" }}
+							>
+								{billdetails}
+								<div class="row row-content">
+									<div class="col-lg-4 col-md-4 col-sm-4 placement">
+										<h4>Item</h4>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-4 placement">
+										<h4>Quantity</h4>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-4 placement">
+										<h4>Price</h4>
+									</div>
+								</div>
+								<hr />
+								<div className="row row-content">{bill}</div>
+								<hr />
+								<div class="row row-content">
+									<div
+										class="
+										placement
+												col-lg-4
+												col-md-4
+												col-sm-4
+												col-xs-4
+											"
+									>
+										TOTAL
+									</div>
+									<div
+										class="
+										placement
+												col-lg-4
+												col-md-4
+												col-sm-4
+												col-xs-4
+											"
+										style={{ marginLeft: "31%" }}
+									>
+										{this.state.total_amount}
+									</div>
+								</div>
+								<div class="card-footer"></div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div>
 					<Link
 						to={{
 							pathname: "/show",
 							state: values,
 						}}
+						className="btn btn-outline-secondary"
+						style={{
+							// backgroundColor: "#fcf93c",
+							marginTop: "15px",
+							marginRight: "15px",
+							marginBottom: "15px",
+						}}
 					>
 						Back to Home page
 					</Link>
 				</div>
-				<button onClick={this.print}>Print</button>
+				<button
+					className="btn btn-outline-secondary"
+					onClick={this.print}
+				>
+					Print
+				</button>
 			</div>
 		);
 	}
